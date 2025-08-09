2e559025061cc7845e7cfbaeda622bdbc78c6a29
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-08-09 18:56:36 +0900

    詳細を表示できるようにした

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "todos#index"
-  get "/todos", to: "todos#index"
+  get "/todos", to: "todos#index", as: "index"
+  get "/todos/:id", to: "todos#show", as: "show"
 end
