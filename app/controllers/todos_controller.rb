25058a031ab8eb1cf48d42689b7dc17ba1c2348e
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-08-10 20:13:55 +0900

    新規作成ができるようにした

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,4 +6,24 @@ class TodosController < ApplicationController
   def show
     @todo = Todo.find(params[:id])
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+
+    if @todo.save
+      redirect_to @todo
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:task, :priority, :deadline)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root "todos#index"
-  get "/todos", to: "todos#index", as: "index"
-  get "/todos/:id", to: "todos#show", as: "show"
+  resources :todos
 end
