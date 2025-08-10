b6d6273fee5d1d717ef80b233438c4939e1ddc12
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-08-10 18:41:51 +0900

    helper を書くコードの場所を移動

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  def priority_label(priority)
-    case priority
-    when 1 then "低"
-    when 2 then "中"
-    when 3 then "高"
-    else "未設定"
-    end
-  end
 end
--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,2 +1,10 @@
 module TodosHelper
+  def priority_label(priority)
+    case priority
+    when 1 then "低"
+    when 2 then "中"
+    when 3 then "高"
+    else "未設定"
+    end
+  end
 end
