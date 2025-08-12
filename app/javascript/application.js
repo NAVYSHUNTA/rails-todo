8f4abb51ad0ea31d0df49050bb1903796609bb0a
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-08-13 05:19:14 +0900

    UI 改善

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,25 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// TODO が完了しているかどうかで見た目を変える
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".check-box").forEach(checkbox => {
+        updateTodoStyle(checkbox);
+
+        checkbox.addEventListener("change", () => {
+            updateTodoStyle(checkbox);
+        });
+    });
+});
+
+function updateTodoStyle(checkbox) {
+    const todo = checkbox.closest(".todo");
+    if (!todo) return;
+    todo.style.backgroundColor = checkbox.checked ? "#bbb" : "";
+
+    const liItems = todo.querySelectorAll("li");
+    liItems.forEach(li => {
+        li.style.textDecoration = checkbox.checked ? "line-through" : "none";
+    });
+}
