f0aabf7b4555146ce5b87909b0ea5a08564b3ce5
Shunta Nakamura <jmsdfnavy.5491@gmail.com>
2025-08-24 00:20:59 +0900

    設定画面にバリデーションを実装した

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,2 +1,5 @@
 class Setting < ApplicationRecord
+  validates :default_deadline_days, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 99 }
+  validates :default_deadline_hours, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 23 }
+  validates :default_deadline_minutes, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 59 }
 end
